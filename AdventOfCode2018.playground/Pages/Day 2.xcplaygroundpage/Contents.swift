532be8030b334548bfc7d2a4fb465069dab3790f
Eric T Cormack <bobryuu@gmail.com>
2018-12-02 11:20:55 -0600

    Day 2, Part 2

--- AdventOfCode2018.playground/Pages/Day 2.xcplaygroundpage/Contents.swift
+++ AdventOfCode2018.playground/Pages/Day 2.xcplaygroundpage/Contents.swift
@@ -2,27 +2,37 @@ import Foundation
 import AdventOfCode2018
 
 guard let filepath = Bundle.main.url(forResource: "02", withExtension: "txt"),
-    let input = StreamReader(url: filepath) else { abort() }
+    let stream = StreamReader(url: filepath) else { abort() }
 let sample = [
-    "abcdef",
-    "bababc",
-    "abbcde",
-    "abcccd",
-    "aabcdd",
-    "abcdee",
-    "ababab"
+    "abcde",
+    "fghij",
+    "klmno",
+    "pqrst",
+    "fguij",
+    "axcye",
+    "wvxyz"
 ]
 
-// Part 1
-let counters = input.map { (line: String) -> [Character: Int] in
-    return line.reduce(into: [Character: Int]()) { $0[$1, default: 0] += 1 }
-}
+let input = stream.filter { $0.count > 0 }
 
-let twos = counters.filter{ (item: [Character: Int]) -> Bool in
-    item.values.contains { $0 == 2 }
-}.count
-let threes = counters.filter{ (item: [Character: Int]) -> Bool in
-    item.values.contains { $0 == 3 }
-}.count
+// Part 1
+let counters = input.map { line in line.reduce(into: [Character: Int]()) { $0[$1, default: 0] += 1 } }
+
+let twos = counters.filter{ item in item.values.contains { $0 == 2 } }.count
+let threes = counters.filter{ item in item.values.contains { $0 == 3 } }.count
 
 print(twos * threes)
+
+// Part 2
+outer: for (i, this) in input.enumerated() {
+    inner: for that in input[(i + 1)...] {
+        let zipper = zip(this, that)
+
+        guard (zipper.filter { $0.0 != $0.1 }).count == 1 else { continue inner }
+
+        print("Box Ids: ", this, that)
+        print("Common letters:", String(zipper.filter { $0.0 == $0.1 }.map { $0.0 }))
+
+        break outer
+    }
+}
