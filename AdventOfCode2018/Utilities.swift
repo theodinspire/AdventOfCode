0293704c73d2f5f8ee06c5ad236b16c35b786512
Eric T Cormack <bobryuu@gmail.com>
2018-12-03 19:28:40 -0600

    Restore XCodeProj

---
+++ AdventOfCode2018/Utilities.swift
@@ -0,0 +1,13 @@
+//
+//  Utilities.swift
+//  AdventOfCode2018
+//
+//  Created by Eric Cormack on 12/1/18.
+//  Copyright © 2018 the Odin Spire. All rights reserved.
+//
+
+import Foundation
+
+public enum Utilities {
+    public static let projectDirectory = "~/git/GitHub/theodinspire/AdventOfCode2018/"
+}
